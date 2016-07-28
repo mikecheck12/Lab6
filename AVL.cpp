d5604864710a06cc220b693f4a4fe339cb7a591e
mikecheck12 <jmichaeloliver12@gmail.com>
2016-07-28 13:30:27 -0600

    Add files via upload

---
+++ AVL.cpp
@@ -0,0 +1,51 @@
+#include "AVL.h"
+#include "LinkedList.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	AVLTreeSet<string> tree;
+	string cmd;
+	string item;
+
+	ifstream in;
+	in.open("in81.txt");
+
+	//ofstream out;
+	//out.open("actual.txt");
+
+	while (in >> cmd) {
+		if (cmd == "clear") {
+			cout << "clear" << endl;
+			tree.clear();
+		}
+		if (cmd == "add") {
+			in >> item;
+			cout << "add " << item << endl;
+			tree.insert(item);
+		}
+		if (cmd == "remove") {
+			in >> item;
+			cout << "remove " << item << endl;
+			tree.remove(item);
+		}
+		if (cmd == "find") {
+			in >> item;
+			cout << "find " << item << " " << tree.find(item) << endl;
+		}
+		if (cmd == "print") {
+			tree.print();			//pass out when writing to file
+		}
+	}
+
+	//out.close();
+	
+
+	system ("pause");
+	return 0;
+}
---
+++ AVL.h
@@ -0,0 +1,200 @@
+#pragma once
+
+#include "LinkedList.h"
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+template <typename ItemType>
+
+class AVLTreeSet {
+
+private:
+
+	struct Node {
+		ItemType item;
+		int height;
+		Node* left;
+		Node* right;
+		Node(const ItemType& data, Node* leftVal = NULL, Node* rightVal = NULL) : item(data), height(1), left(leftVal), right(rightVal) {}
+
+		~Node() {};
+	};
+
+
+	Node* root;
+
+	void clearTree(Node* root) {
+		if (root == NULL) {
+			return;
+		}
+		else {
+			clearTree(root->left);
+			clearTree(root->right);
+			delete(root);
+			root = NULL;
+		}
+	}
+
+public:
+	AVLTreeSet() {
+		root = NULL;
+	}
+
+	~AVLTreeSet() { clear(); };
+
+//GET HEIGHT FUNCTION --------------------------------
+
+	int getHeight(Node* n) {
+		if (n == NULL) {
+			return 0;
+		}
+		else {
+			return n->height;
+		}
+	}
+
+//SET HEIGHT FUNCTION ---------------------------------
+
+	void setHeight(Node* n) {
+		if (getHeight(n->right) > getHeight(n->left)) {
+			n->height = getHeight(n->right) + 1;
+		}
+		else {
+			n->height = getHeight(n->left) + 1;
+		}
+	}
+
+//INSERT FUNCTION ------------------------------------
+
+	void insert(const ItemType& item) {
+		insert(root, item);
+	}
+
+	bool insert(Node*& n, const ItemType& item) {				//edit function to account for names alreqady included in the tree
+		if (n == NULL) {
+			n = new Node(item);
+			return true;
+		}
+		else if (item < n->item) {
+			if (insert(n->left, item)) {
+				setHeight(n);					
+				return true;
+			}
+		}
+		else if (item > n->item) {
+			if (insert(n->right, item)) {
+				setHeight(n);					
+				return true;
+			}
+		}
+		else {
+			return false;
+		}
+
+	}
+
+//FIND MIN FUNCTION ---------------------------------
+
+	ItemType findMin(Node* n) const {
+		if (n->left == NULL) {
+			return n->item;
+		}
+		else {
+			return findMin(n->left);
+		}
+	}
+
+//REMOVE FUNCTION -----------------------------------
+
+	void  remove(const ItemType& item) {
+		remove(root, item);
+	}
+
+	bool remove(Node*& n, const ItemType& item) {
+		if (n == NULL) {
+			return false;
+		}
+		else if (item < n->item) {
+			remove(n->left, item);
+			setHeight(n);
+			return true;
+		}
+		else if (item > n->item) {
+			remove(n->right, item);
+			setHeight(n);
+			return true;
+		}
+		else {
+			Node* removeNode = n;
+			if (n->left == NULL) {
+				n = n->right;
+			}
+			else if (n->right == NULL) {
+				n = n->left;
+			}
+			else {
+				n->item = findMin(n->right);
+				remove(n->right, n->item);
+			}
+			delete removeNode;
+			return true;
+		}
+
+	}
+
+//FIND FUNCTION --------------------------------
+	bool find(const ItemType& item) {
+
+		return false;
+
+	}
+
+//CLEAR WRAPPER FUNCTION --------------------------
+
+	void clear() {
+		clearTree(root);
+	}
+
+//PRINT FUNCTION ---------------------------------
+
+	void print() {
+		int queueSize;
+		int level = 0;
+		int max = 8;
+		LinkedList<Node*> printQueue;
+
+		cout << "print" << endl;
+
+		if (root != NULL) {
+			printQueue.push_back(root);
+			while (printQueue.getSize() != 0) {				
+				queueSize = printQueue.getSize();
+				cout << "level " << level << ": ";
+				for (int i = 0; i < queueSize; i++) {
+					if (queueSize > max) {
+						cout << endl;
+					}
+					auto item = printQueue.pop_front();				//where do I add children as it will effect the size
+						if (item->right != NULL) {
+							printQueue.push_back(item->right);
+						}
+						if (item->left != NULL) {
+							printQueue.push_back(item->left);
+						}
+						cout << item->item << " (" << getHeight(item) << ")";
+					//cout << printQueue.pop_front() << " " << getHeight(i->item);
+
+				}
+				level++;
+				cout << endl;
+			}
+		}
+	}
+		
+
+
+};
+
